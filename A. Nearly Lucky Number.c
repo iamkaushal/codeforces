3f9301a090e625e1dd671ddfa8331b8e26495072
iamkaushal <kaushalsharma880@gmail.com>
2017-11-06 00:55:53 +0530

    solved A. Nearly Lucky Number

---
+++ A. Nearly Lucky Number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    char str[100];
+    scanf("%s", str);
+    int luckydigit=0, unluckydigit=0;
+    for (int i=0, n=strlen(str); i<n;i++)
+    {
+        if(str[i]=='4'||str[i]=='7') luckydigit++;
+        else unluckydigit++;
+    }
+    /*
+    if(unluckydigit==0)
+    {
+         printf("YES");
+         return 0;
+    }
+    */
+    if(luckydigit==4||luckydigit==7) printf("YES");
+    else printf("NO");
+
+    return 0;
+}
