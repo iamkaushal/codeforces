ef8d2e281c56107b65a3c55aaab68c40711b898f
iamkaushal <kaushalsharma880@gmail.com>
2018-01-12 23:37:08 +0530

    solved helpul maths.c

--- A. Helpful Maths.c
+++ A. Helpful Maths.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<strings.h>
 
 int main()
 {
     char arr[102];
 
-    //scanf("%s", arr[0]);
-    gets(arr);
+    scanf("%s", &arr);
 
     int sum1=0;
     int sum2=0;
     int sum3=0;
+
     int n= strlen(arr);
 
-    printf("%d", n);
+
+
 
     for(int i=0; i<n;i++)
     {
@@ -23,32 +25,30 @@ int main()
         if(arr[i]=='3') sum3++;
     }
 
-    printf("%d %d %d", sum1, sum2, sum3);
+   // printf("%d %d %d \n", sum1, sum2, sum3);
 
-    for(int i=0; i<n;)
-    {
-        while(sum1--)
+  int i=0;
+         while(sum1--)
         {
             if(i%2==0) arr[i] = '1';
-            else arr[i] = '+';
-            i++;
+            //else arr[i] = '+';
+            i= i+2;
         }
+
 
         while(sum2--)
         {
             if(i%2==0) arr[i] = '2';
-            else arr[i] = '+';
-            i++;
+            //else arr[i] = '+';
+            i= i+2;
         }
 
         while(sum3--)
         {
             if(i%2==0) arr[i] = '3';
-            else arr[i] = '+';
-            i++;
+            //else arr[i] = '+';
+            i= i+2;
         }
-
-    }
 
     printf("%s", arr);
 
---
+++ test.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<strings.h>
+
+int main()
+{
+    char arr[102];
+
+    scanf("%s", &arr);
+    printf("%s\n", arr);
+
+    int n= strlen(arr);
+    printf("%d\n", n);
+
+
+
+
+    return 0;
+
+}
